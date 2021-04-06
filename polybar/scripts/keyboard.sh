6065a15d303c6dbd3adac10064eacee4af8c5380
Joaquin Hoyos <joaco.hoyos@hotmail.com>
2021-04-05 23:36:29 -0300

    add intl dvorak

--- polybar/scripts/keyboard.sh
+++ polybar/scripts/keyboard.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-layoutUS=$(setxkbmap -query | grep intl);
+layoutDvorakIntl=$(setxkbmap -query | grep dvorak-intl);
 layoutLatam=$(setxkbmap -query | grep latam);
-if [ -z "$layoutUS" ]
+if [ -z "$layoutDvorakIntl" ]
 then
   if [ -z "$layoutLatam" ]
   then
-    setxkbmap -layout latam -option caps:swapescape;
+    setxkbmap -layout latam -option;
   else
-    setxkbmap -layout us -variant intl -option caps:swapescape;
+    setxkbmap -layout us -variant dvorak-intl -option;
   fi
 else 
   setxkbmap -layout us -variant dvp -option;
