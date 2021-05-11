0922437e4cda059bd0c3e6c4146b2df5125747b7
Joaquin Hoyos <joaco.hoyos@hotmail.com>
2021-05-10 23:22:31 -0300

    fix small bugs in worktree

--- nvim/lua/clarkyex/git-worktree.lua
+++ nvim/lua/clarkyex/git-worktree.lua
@@ -1,6 +1,4 @@
 local Worktree = require("git-worktree")
-local Job = require("plenary.job")
-local Path = require("plenary.path")
 
 local function is_pc_api()
     return not not (string.find(vim.loop.cwd(), vim.env.PC_PATH .. "/api",1, true))
@@ -18,40 +16,55 @@ local function kill_window(session_name)
     os.execute(string.format("tmux kill-window -t %s", session_name))
 end
 
+local function is_absolute_path(path)
+  return not not (string.find(path, "/", 1, true))
+end
+
 local function add_window(session, window, dir, cmd)
     os.execute(string.format("tmux neww -t %s: -n %s -d  -c %s", session, window , dir))
     os.execute(string.format("tmux send-keys -t %s:%s \"%s\" C-m",session,window, cmd))
 end
 
+local function get_dir(path, project)
+  if is_absolute_path(path) then
+     return path
+  end
+  return vim.env.PC_PATH .. "/" .. project .. "/" .. path
+end
+
 Worktree.on_tree_change(function(op, path, upstream)
+    dir = path["path"]
     if op == Worktree.Operations.Switch and is_pc_api() then
       kill_window("peer:run-api")
-      add_window("peer", "run-api", path["path"], "docker-compose -f docker/docker-compose.yml up --build")
+      tree_dir = get_dir(dir, "api")
+      add_window("peer", "run-api", tree_dir, "docker-compose -f docker/docker-compose.yml up --build")
     end
 
     if op == Worktree.Operations.Switch and is_pc_app() then
+      tree_dir = get_dir(dir, "app")
       kill_window("peer:run-app")
-      add_window("peer", "run-app", path["path"], "npm start")
+      add_window("peer", "run-app", tree_dir, "npm start")
     end
 
     if op == Worktree.Operations.Switch and is_pc_www() then
+      tree_dir = get_dir(dir, "www")
       kill_window("peer:run-www")
-      add_window("peer", "run-www", path["path"], "npm start")
+      add_window("peer", "run-www", tree_dir, "npm start")
     end
 
     if op == Worktree.Operations.Create and is_pc_www() then
-        os.execute(string.format("cp -r $PC_PATH/www/master/node_modules $PC_PATH/www/%s", path["path"]))
-        os.execute(string.format("cp -r $PC_PATH/www/master/.env.development $PC_PATH/www/%s", path["path"]))
+        os.execute(string.format("cp -r $PC_PATH/www/master/node_modules $PC_PATH/www/%s", dir))
+        os.execute(string.format("cp -r $PC_PATH/www/master/.env.development $PC_PATH/www/%s", dir))
     end
 
     if op == Worktree.Operations.Create and is_pc_app() then
-        os.execute(string.format("cp -r $PC_PATH/app/master/node_modules $PC_PATH/app/%s", path["path"]))
-        os.execute(string.format("cp -r $PC_PATH/app/master/.env $PC_PATH/app/%s", path["path"]))
+        os.execute(string.format("cp -r $PC_PATH/app/master/node_modules $PC_PATH/app/%s", dir))
+        os.execute(string.format("cp -r $PC_PATH/app/master/.env $PC_PATH/app/%s", dir))
     end
 
     if op == Worktree.Operations.Create and is_pc_api() then
-        os.execute(string.format("cp -r $PC_PATH/api/master/.env $PC_PATH/api/%s", path["path"]))
-        os.execute(string.format("cp -r  $PC_PATH/api/%s/config/database.sample.yml $PC_PATH/api/%s/config/database.yml", path["path"], path["path"]))
+        os.execute(string.format("cp -r $PC_PATH/api/master/.env $PC_PATH/api/%s", dir))
+        os.execute(string.format("cp -r  $PC_PATH/api/%s/config/database.sample.yml $PC_PATH/api/%s/config/database.yml", dir, dir))
     end
 
 end)
