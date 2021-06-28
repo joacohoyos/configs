8f604124e0829431feadf38f9e150655486dd76a
Joaquin Hoyos <joaco.hoyos@hotmail.com>
2021-06-27 22:09:29 -0300

    commit.lua changes

--- nvim/lua/helpers/commit.lua
+++ nvim/lua/helpers/commit.lua
@@ -2,7 +2,11 @@ local commit = {}
 
 function commit.command()
   message = vim.fn.input("Commit message: ")
+  if message == "" then
+    cmd = "git commit \n"
+  else
   cmd = "gcm \"" .. message .. "\"\n"
+  end
   require("harpoon.term").sendCommand(1 ,cmd)
   require("harpoon.term").gotoTerminal(1)
 end
